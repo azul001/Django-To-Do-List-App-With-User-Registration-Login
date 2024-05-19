5369a62d72e1b1b9a9cc8d80d1ffcf7691cd74e5
prince markose <princemarkose2002@gmail.com>
2024-05-19 14:37:18 -0700

    first commit

---
+++ base/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
